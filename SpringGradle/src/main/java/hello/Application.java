933f95da089f5d41b1e7ea23436bc97ed95350af
shad0w77 <jasonemeric@gmail.com>
2014-07-19 13:23:56 -0400

    form validation

--- SpringGradle/src/main/java/hello/Application.java
+++ SpringGradle/src/main/java/hello/Application.java
@@ -1,14 +1,17 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
