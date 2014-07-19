c5c6460178aab0c049fb4d82ae9107f2aaaba8f9
shad0w77 <jasonemeric@gmail.com>
2014-07-19 10:04:18 -0400

    booking service transaction management

--- SpringGradle/src/main/java/hello/Application.java
+++ SpringGradle/src/main/java/hello/Application.java
@@ -1,16 +1,75 @@
 package hello;
 
-import org.springframework.web.client.RestTemplate;
+import javax.sql.DataSource;
 
+import org.junit.Assert;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAutoConfiguration
 public class Application {
 
-    public static void main(String args[]) {
-        RestTemplate restTemplate = new RestTemplate();
-        Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
-        System.out.println("Name:    " + page.getName());
-        System.out.println("About:   " + page.getAbout());
-        System.out.println("Phone:   " + page.getPhone());
-        System.out.println("Website: " + page.getWebsite());
+    @Bean
+    BookingService bookingService() {
+        return new BookingService();
+    }
+
+    @Bean
+    DataSource dataSource() {
+        return new SimpleDriverDataSource() {{
+            setDriverClass(org.h2.Driver.class);
+            setUsername("sa");
+            setUrl("jdbc:h2:mem");
+            setPassword("");
+        }};
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table BOOKINGS if exists");
+        jdbcTemplate.execute("create table BOOKINGS(" +
+                "ID serial, FIRST_NAME varchar(5) NOT NULL)");
+        return jdbcTemplate;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        BookingService bookingService = ctx.getBean(BookingService.class);
+        bookingService.book("Alice", "Bob", "Carol");
+        Assert.assertEquals("First booking should work with no problem",
+                3, bookingService.findAllBookings().size());
+
+        try {
+            bookingService.book("Chris", "Samuel");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Assert.assertEquals("'Samuel' should have triggered a rollback",
+                3, bookingService.findAllBookings().size());
+
+        try {
+            bookingService.book("Buddy", null);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Assert.assertEquals("'null' should have triggered a rollback",
+                3, bookingService.findAllBookings().size());
+        
+        System.out.println("FIND_ALL_BOOKINGS = " + bookingService.findAllBookings());
+
     }
 
 }
