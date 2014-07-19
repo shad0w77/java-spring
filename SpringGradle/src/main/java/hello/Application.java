03b3be98ebf879ce12f70971f608c0dba6eff653
shad0w77 <jasonemeric@gmail.com>
2014-07-19 09:49:36 -0400

    consume rest service

--- SpringGradle/src/main/java/hello/Application.java
+++ SpringGradle/src/main/java/hello/Application.java
@@ -1,14 +1,16 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
-@ComponentScan
-@EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
+        System.out.println("Name:    " + page.getName());
+        System.out.println("About:   " + page.getAbout());
+        System.out.println("Phone:   " + page.getPhone());
+        System.out.println("Website: " + page.getWebsite());
     }
+
 }
